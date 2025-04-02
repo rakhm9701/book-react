2e016025a9ccad055a1b4cbbdf85855808df0341
David <odilovrakhmidin.gmail.com>
2025-04-02 12:15:43 +0900

    fix midify deploy. sh file

--- deploy.sh
+++ deploy.sh
@@ -10,8 +10,6 @@ yarn global add serve
 yarn
 yarn run build
 pm2 start "yarn run start:prod"  --name=BOOKSAW-REACT
-pm2 stop BOOKSAW-REACT # Agar allaqachon ishga tushirilgan bo'lsa
-pm2 start "serve -s build -l 1004 --single" --name=BOOKSAW-REACT
 
 
 
